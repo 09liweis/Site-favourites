48cc0cb12e0e47fc7b66078682362df5616325f6
samliweisen <liweisen007@gmail.com>
2017-06-03 17:05:37 +0000

    remove add new url on header

--- app/assets/javascripts/components/user_profile.es6.jsx
+++ app/assets/javascripts/components/user_profile.es6.jsx
@@ -49,6 +49,7 @@ class UserProfile extends React.Component {
         <nav className="nav block">
           <a onClick={this.changeView.bind(this, 'owned')}><i className="fa fa-user fa-fw" aria-hidden="true"></i> My Urls</a>
           <a onClick={this.changeView.bind(this, 'favourites')}><i className="fa fa-star fa-fw" aria-hidden="true"></i> My Favourites</a>
+          <a onClick={this.changeView.bind(this, 'add_new')}><i className="fa fa-star fa-fw" aria-hidden="true"></i> Add New</a>
         </nav>
         {(this.state.view == 'owned') ? 
         <UrlList urls={this.state.ownUrls} displayDetail={this.props.displayDetail} />
@@ -56,6 +57,10 @@ class UserProfile extends React.Component {
         
         {(this.state.view == 'favourites') ? 
         <UrlList urls={this.state.favourites} displayDetail={this.props.displayDetail} />
+        : '' }
+        
+        {(this.state.view == 'add_new') ? 
+        <UrlForm />
         : '' }
         
       </div>
