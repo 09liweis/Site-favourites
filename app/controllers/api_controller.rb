3e765434ea03961e64888fbc2160f37f88d72736
samliweisen <liweisen007@gmail.com>
2017-12-29 16:00:53 +0000

    add allow cros on api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,15 @@
 class ApiController < ApplicationController
     skip_before_filter  :verify_authenticity_token
+    after_filter :cors_set_access_control_headers
+    
+    def cors_set_access_control_headers
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+        headers['Access-Control-Request-Method'] = '*'
+        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    end
+    
+    
     def login
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
