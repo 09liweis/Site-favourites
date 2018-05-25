394a6f2c1393cd0eb44edde9af929ad3efbd514b
samliweisen <liweisen007@gmail.com>
2018-05-25 15:56:50 +0000

    style url element

--- app/assets/javascripts/components/url.es6.jsx
+++ app/assets/javascripts/components/url.es6.jsx
@@ -10,15 +10,14 @@ class Url extends React.Component {
     let action;
     
     if (this.props.page == 'profile') {
-      action = <a className="f-left link btn" onClick={this.props.handleUrl.bind(this, url)}>Remove</a>;
+      action = <a className="link btn" onClick={this.props.handleUrl.bind(this, url)}>Remove</a>;
     }
     return (
       <div className="url">
-        <img className="f-left favicon" src={url.favicon} />
-        <a className="f-left link" onClick={this.props.displayDetail.bind(this, url)}>{url.title}</a>
-        <a className="f-left link" href={url.link} target="_blank"><i className="fa fa-external-link"></i></a>
+        <img className="favicon" src={url.favicon} />
+        <a className="link" onClick={this.props.displayDetail.bind(this, url)}>{url.title}</a>
+        <a className="link" href={url.link} target="_blank"><i className="fa fa-external-link"></i></a>
         {action}
-        <div className="clear"></div>
       </div>
     );
   }
