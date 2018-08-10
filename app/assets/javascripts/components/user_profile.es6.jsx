05871df034f170baa118c8675dabe5d3177efd5e
samliweisen <liweisen007@gmail.com>
2018-08-10 16:07:42 +0000

    refactor user profile ownUrls and favourites

--- app/assets/javascripts/components/user_profile.es6.jsx
+++ app/assets/javascripts/components/user_profile.es6.jsx
@@ -43,20 +43,20 @@ class UserProfile extends React.Component {
     });
   }
   render () {
-    const {view} = this.state;
+    const {view, ownUrls, favourites} = this.state;
     return (
       <div>
         <nav className="nav block">
-          <a className={(view == 'owned' ? 'selected' : '')} onClick={this.changeView.bind(this, 'owned')}><i className="fa fa-user fa-fw" aria-hidden="true"></i> My Urls {this.state.ownUrls.length}</a>
-          <a className={(view == 'favourites' ? 'selected' : '')} onClick={this.changeView.bind(this, 'favourites')}><i className="fa fa-star fa-fw" aria-hidden="true"></i> My Favourites {this.state.favourites.length}</a>
+          <a className={(view == 'owned' ? 'selected' : '')} onClick={this.changeView.bind(this, 'owned')}><i className="fa fa-user fa-fw" aria-hidden="true"></i> My Urls {ownUrls.length}</a>
+          <a className={(view == 'favourites' ? 'selected' : '')} onClick={this.changeView.bind(this, 'favourites')}><i className="fa fa-star fa-fw" aria-hidden="true"></i> My Favourites {favourites.length}</a>
           <a className={(view == 'add_new' ? 'selected' : '')} onClick={this.changeView.bind(this, 'add_new')}><i className="fa fa-plus fa-fw" aria-hidden="true"></i> Add New</a>
         </nav>
         {(view == 'owned') ? 
-        <UrlList page="profile" urls={this.state.ownUrls} displayDetail={this.props.displayDetail} />
+        <UrlList page="profile" urls={ownUrls} displayDetail={this.props.displayDetail} />
         : '' }
         
         {(view == 'favourites') ? 
-        <UrlList urls={this.state.favourites} displayDetail={this.props.displayDetail} />
+        <UrlList urls={favourites} displayDetail={this.props.displayDetail} />
         : '' }
         
         {(view == 'add_new') ? 
