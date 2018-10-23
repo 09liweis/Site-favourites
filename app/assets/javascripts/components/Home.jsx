f1ac3cf951ca21d43cf0a31d6780af46106fc86e
samliweisen <liweisen007@gmail.com>
2018-10-23 16:55:37 +0000

    create new home component

---
+++ app/assets/javascripts/components/Home.jsx
@@ -0,0 +1,17 @@
+class Home extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            tags: [],
+            urls: [],
+            selectedTag: 'all'
+        };
+    }
+    render () {
+        return (
+          <div>
+          </div>
+        );
+    }
+}
+
